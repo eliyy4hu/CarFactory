b806309944ff82817d8dc5125ab5ec97637812a0
eliyy4hu <eliyy4hu@outlook.com>
2019-12-03 14:39:41 +0500

    add usage example

--- CarFactory/Program.cs
+++ CarFactory/Program.cs
@@ -1,14 +1,19 @@
-﻿namespace CarFactory
+﻿using System;
+
+namespace CarFactory
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var audiFac = new AUDICarFactory();
+            var audiBody = audiFac.CreateBody();
+            var audiEngine = audiFac.CreateEngine();
+            var audiInterior = audiFac.CreateInterior();
+            var bmwFac = new BMWCarFactory();
+            var bmwBody = bmwFac.CreateBody();
+            var bmwEngine = bmwFac.CreateEngine();
+            var bmwInterior = bmwFac.CreateInterior();
         }
     }
-
-    
-    
-
-    
 }
